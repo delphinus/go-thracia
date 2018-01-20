26e573995167b29e1402b0cac0a77c1c63bb5723
delphinus <delphinus@remora.cx>
2018-01-20 20:36:54 +0900

    Add logic to execute scripts

--- scripts.go
+++ scripts.go
@@ -16,7 +16,7 @@ func scripts(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("cannot find `fontforge` executable")
 	}
-	if err := generateScripts(ctx, modifyMigu1mTmpl, h{
+	if script, err := generateScripts(ctx, modifyMigu1mTmpl, h{
 		"FontForge":  fontforge,
 		"SrcRegular": migu1mTTFs[0],
 		"SrcBold":    migu1mTTFs[1],
@@ -26,6 +26,8 @@ func scripts(ctx context.Context) error {
 		"DstDir":     tempDir(ctx),
 	}); err != nil {
 		return fmt.Errorf("error in generateScripts: %v", err)
+	} else if err := execScripts(ctx, script); err != nil {
+		return fmt.Errorf("error in execScripts: %v", err)
 	}
 	return nil
 }
@@ -35,27 +37,39 @@ func scriptFilename(ctx context.Context, tmpl string) string {
 	return pathInTempDir(ctx, filename)
 }
 
-func generateScripts(ctx context.Context, tmpl string, data interface{}) (err error) {
+func generateScripts(ctx context.Context, tmpl string, data interface{}) (script string, err error) {
 	f, err := Assets.Open(tmpl)
 	if err != nil {
-		return fmt.Errorf("error in Open: %v", err)
+		return "", fmt.Errorf("error in Open: %v", err)
 	}
 	defer checkClose(f, &err)
 	content, err := ioutil.ReadAll(f)
 	if err != nil {
-		return fmt.Errorf("error in ReadAll: %v", err)
+		return "", fmt.Errorf("error in ReadAll: %v", err)
 	}
 	t, err := template.New("").Parse(string(content))
 	if err != nil {
-		return fmt.Errorf("error in Parse: %v", err)
+		return "", fmt.Errorf("error in Parse: %v", err)
 	}
-	dst, err := os.Create(scriptFilename(ctx, tmpl))
+	script = scriptFilename(ctx, tmpl)
+	dst, err := os.OpenFile(script, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
-		return fmt.Errorf("error in Create: %v", err)
+		return "", fmt.Errorf("error in Create: %v", err)
 	}
 	defer checkClose(dst, &err)
 	if err := t.Execute(dst, data); err != nil {
-		return fmt.Errorf("error in Execute: %v", err)
+		return "", fmt.Errorf("error in Execute: %v", err)
+	}
+	return
+}
+
+func execScripts(ctx context.Context, script string) error {
+	c := cliContext(ctx)
+	cmd := exec.CommandContext(ctx, script)
+	cmd.Stdout = c.App.Writer
+	cmd.Stderr = c.App.ErrWriter
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error in Run: %v", err)
 	}
 	return nil
 }
